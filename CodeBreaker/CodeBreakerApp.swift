f95a0eb7cd6123b69c9b4970b16eb60e649c4626
Mokhtari Hadj Mohamed <7adj.mo7amed@gmail.com>
2025-12-02 21:13:08 +0100

    init project and some code

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Hadj Mohamed Mokhtari on 1/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {  
+            ContentView()
+        }
+    }
+}
